59624e17587a53a4c537b3d0e2fa069922ea7eb5
Jacob <ecstaticjack@gmail.com>
2014-09-15 05:55:46 -0400

    Create MENU.h
    I was working on this for something

---
+++ Applications/MENU.h
@@ -0,0 +1,22 @@
+/*
+ * MENU.h
+ *
+ * Created: 8/8/2014 4:07:28 PM
+ *  Author: jacob_000
+ */ 
+
+
+#ifndef MENU_H_
+#define MENU_H_
+
+#include <avr/io.h>
+#include "INIT_CS1_SRAM_LCD.h"
+#include "inputCapture.h"
+#include "PezioSpeaker_INIT.h"
+#include "delay.h"
+
+
+
+
+
+#endif /* MENU_H_ */
